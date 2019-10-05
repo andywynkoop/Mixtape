3f1e89d15ad38903df7f71a39aa5a3535bb6569e
Andy Wynkoop <andywynkoop@gmail.com>
2019-10-04 22:46:48 -0700

    Try to add environment variables into frontend

--- react/actions/keys.js
+++ react/actions/keys.js
@@ -1,7 +1,6 @@
 let API_KEY;
-debugger;
-if (process.env.NODE_ENV === 'production') {
-	API_KEY = process.env.GOOGLE_API_KEY;
+if (NODE_ENV === 'production') {
+	API_KEY = GOOGLE_API_KEY;
 } else {
 	API_KEY = require('./secret');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
-console.log(process.env.NODE_ENV);
-const NODE_ENV = process.env.NODE_ENV;
-const prod = NODE_ENV === 'production';
-const NODE_ENDPOINT = prod
-	? process.env.NODE_ENDPOINT
-	: 'http://localhost:3001';
-const RAILS_ENDPOINT = prod
-	? process.env.RAILS_ENDPOINT
-	: 'http://localhost:3000';
-const GOOGLE_API_KEY = prod ? process.env.GOOGLE_API_KEY : null;
 
 module.exports = {
 	entry: './react/index.js',
@@ -26,15 +15,7 @@ module.exports = {
 					loader: 'babel-loader',
 					query: {
 						presets: ['@babel/env', '@babel/react'],
-						plugins: [
-							'@babel/proposal-class-properties',
-							new webpack.DefinePlugin({
-								NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-								NODE_ENDPOINT: JSON.stringify(NODE_ENDPOINT),
-								RAILS_ENDPOINT: JSON.stringify(RAILS_ENDPOINT),
-								GOOGLE_API_KEY: JSON.stringify(GOOGLE_API_KEY),
-							}),
-						],
+						plugins: ['@babel/proposal-class-properties'],
 					},
 				},
 			},
